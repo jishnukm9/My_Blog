e972cbd8c00fd4697407d0328d4eb06fc364f32d
Jishnu Km <jishnukm2010@gmail.com>
2022-03-05 01:45:05 +0530

    second commit

--- forms.py
+++ forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms.validators import DataRequired, URL
 from flask_ckeditor import CKEditorField
-import email_validator
+
 
 ##WTForm
 class CreatePostForm(FlaskForm):
